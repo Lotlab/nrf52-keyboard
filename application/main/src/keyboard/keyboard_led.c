2fd1e9a70bd062028ac2a981bcbc2dc9c9825b91
Geno [格诺] <genokolar@gmail.com>
2019-06-22 15:29:02 +0800

    添加独立的系统状态指示灯；添加LED显示功能
    1、RGB灯需要占用3个pin针脚，同时以颜色来做状态提示需要记忆颜色对应的状态；
    此处添加独立的系统状态指示灯功能，作为一种选择。
    2、由于蓝牙状态，状态灯为了节能会自动熄灭，led_display功能可以在状态灯熄灭时，按键主动开启状态灯显示，以便获取系统运行状态。

--- application/main/src/keyboard/keyboard_led.c
+++ application/main/src/keyboard/keyboard_led.c
@@ -20,6 +20,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "app_timer.h"
 #include "nrf.h"
 #include "nrf_gpio.h"
+#include "nrf_delay.h"
 
 static void off_timer_start();
 
@@ -31,7 +32,7 @@ static low_power_pwm_t led_g;
 static low_power_pwm_t led_b;
 static uint32_t saved_color;
 
-static void pwm_handler(void* p_context)
+static void pwm_handler(void *p_context)
 {
     UNUSED_PARAMETER(p_context);
 }
@@ -53,8 +54,7 @@ const uint8_t gamma[] = {
     68, 70, 71, 73, 75, 76, 78, 80,
     81, 83, 85, 87, 88, 90, 92, 94,
     96, 98, 100, 102, 104, 106, 108, 110,
-    112, 114, 116, 118, 120, 122, 124, 127
-};
+    112, 114, 116, 118, 120, 122, 124, 127};
 
 /**
  * @brief 设置RGB灯光值
@@ -73,8 +73,8 @@ static void keyboard_led_rgb_set_internal(uint32_t color)
     b = gamma[b];
 
     // 红色灯光的亮度在同样电流下是其他两个的一半，故需要校正颜色
-    low_power_pwm_duty_set(&led_r, r * 2); 
-    low_power_pwm_duty_set(&led_g, g); 
+    low_power_pwm_duty_set(&led_r, r * 2);
+    low_power_pwm_duty_set(&led_g, g);
     low_power_pwm_duty_set(&led_b, b);
 }
 
@@ -161,51 +161,127 @@ void keyboard_led_rgb_direct(uint8_t bit)
 #endif
 
 /**
- * @brief 设置LED灯的值
+ * @brief 设置键盘状态LED灯的值
  * 
- * @param led_val 
+ * @param keyboard_led_val   LED状态值
  */
-static void keyboard_led_set_internal(uint8_t led_val)
+static void keyboard_led_set_internal(uint8_t keyboard_led_val)
 {
 #ifdef LED_NUM
-    LED_WRITE(LED_NUM, led_val & (1 << 0));
+    LED_WRITE(LED_NUM, keyboard_led_val & (1 << 0));
 #endif
 #ifdef LED_CAPS
-    LED_WRITE(LED_CAPS, led_val & (1 << 1));
+    LED_WRITE(LED_CAPS, keyboard_led_val & (1 << 1));
 #endif
 #ifdef LED_SCLK
-    LED_WRITE(LED_SCLK, led_val & (1 << 2));
+    LED_WRITE(LED_SCLK, keyboard_led_val & (1 << 2));
 #endif
 }
 
-static uint8_t saved_led_val;
+static uint8_t saved_keyboard_led_val;
 
 /**
- * @brief 设置LED灯的值并启用自动熄灭
+ * @brief 设置键盘状态LED灯的值并启用自动熄灭
  * 
- * @param led_val 
+ * @param keyboard_led_val   LED状态值
  */
-void keyboard_led_set(uint8_t led_val)
+void keyboard_led_set(uint8_t keyboard_led_val)
 {
-    saved_led_val = led_val;
-    keyboard_led_set_internal(led_val);
+    saved_keyboard_led_val = keyboard_led_val;
+    keyboard_led_set_internal(keyboard_led_val);
     off_timer_start();
 }
 
+#ifdef LED_STATUS
+
 /**
- * @brief 将LED熄灭
+ * @brief 设置系统状态LED灯的状态
+ * 
+ * @param system_led_val   LED状态值
+ */
+static void system_led_set_internal(uint8_t system_led_val)
+{
+//更改系统LED的状态
+#ifdef LED_BLE
+    LED_WRITE(LED_BLE, system_led_val & (1 << LED_BIT_BLE));
+#endif
+#ifdef LED_USB
+    LED_WRITE(LED_USB, system_led_val & (1 << LED_BIT_USB));
+#endif
+#ifdef LED_CHARGING
+    LED_WRITE(LED_CHARGING, system_led_val & (1 << LED_BIT_CHARGING));
+#endif
+}
+
+/**
+ * @brief 开启系统状态LED灯
+ * 
+ */
+void system_led_on(void)
+{
+//开启LED状态灯
+#ifdef LED_BLE
+    LED_WRITE(LED_BLE, 1);
+#endif
+#ifdef LED_USB
+    LED_WRITE(LED_USB, 1);
+#endif
+#ifdef LED_CHARGING
+    LED_WRITE(LED_CHARGING, 1);
+#endif
+}
+
+uint8_t saved_system_led_val;
+
+/**
+ * @brief 设置系统状态LED灯的状态值
+ * 
+ * @param bit 位置
+ * @param state 状态
+ */
+void system_led_set_val(enum led_bit_usage bit, bool state)
+{
+    uint8_t system_led_val = saved_system_led_val; //读取系统LED的状态值
+    // 更改系统LED的状态的值
+    if (state)
+    {
+        system_led_val |= 1 << bit;
+    }
+    else
+    {
+        system_led_val &= ~(1 << bit);
+    }
+    saved_system_led_val = system_led_val; //存储系统LED的状态值
+}
+
+/**
+ * @brief 设置系统状态LED灯,并启用自动熄灭
+ * 
+ */
+void system_led_set()
+{
+    system_led_set_internal(saved_system_led_val);
+    off_timer_start();
+}
+#endif
+
+/**
+ * @brief 将所有LED、RGB熄灭
  * 
  */
 static void led_off(void)
 {
     keyboard_led_set_internal(0);
+#ifdef LED_STATUS
+    system_led_set_internal(0);
+#endif
 #ifdef LED_RGB
     keyboard_led_rgb_set_internal(0);
 #endif
 }
 
 /**
- * @brief 将LED熄灭
+ * @brief 将所有LED、RGB熄灭、并停用RGB。
  * 
  */
 void keyboard_led_off(void)
@@ -221,14 +297,18 @@ void keyboard_led_off(void)
 static bool counting;
 static bool led_autooff = true;
 APP_TIMER_DEF(led_off_timer);
+APP_TIMER_DEF(led_blink_timer);
 
 /**
- * @brief 将LED点亮
+ * @brief 重新启用LED、RGB，并恢复原本状态
  * 
  */
 static void led_on(void)
 {
-    keyboard_led_set_internal(saved_led_val);
+    keyboard_led_set_internal(saved_keyboard_led_val);
+#ifdef LED_STATUS
+    system_led_set_internal(saved_system_led_val);
+#endif
 #ifdef LED_RGB
     keyboard_led_rgb_set_internal(saved_color);
 #endif
@@ -239,10 +319,22 @@ static void led_on(void)
  * 
  * @param context 
  */
-static void led_off_timer_handler(void* context)
+static void led_off_timer_handler(void *context)
 {
     led_off();
     counting = false;
+}
+
+/**
+ * @brief LED闪烁并提示状态handler
+ * 
+ * @param context 
+ */
+static void led_blink_timer_handler(void *context)
+{
+    led_off();
+    led_on();
+    off_timer_start();
 }
 
 /**
@@ -251,7 +343,8 @@ static void led_off_timer_handler(void* context)
  */
 static void off_timer_start()
 {
-    if (led_autooff) {
+    if (led_autooff)
+    {
         if (counting)
             app_timer_stop(led_off_timer);
         led_on();
@@ -261,12 +354,13 @@ static void off_timer_start()
 }
 
 /**
- * @brief 初始计时器
+ * @brief 初始自动关灯计时器
  * 
  */
 static void off_timer_init(void)
 {
     app_timer_create(&led_off_timer, APP_TIMER_MODE_SINGLE_SHOT, led_off_timer_handler);
+    app_timer_create(&led_blink_timer, APP_TIMER_MODE_SINGLE_SHOT, led_blink_timer_handler);
 }
 
 /**
@@ -286,6 +380,21 @@ void keyboard_led_powersave(bool powersave)
         led_on();
 }
 
+/**
+ * @brief 启用LED状态灯显示
+ */
+void led_display()
+{
+    keyboard_led_set_internal(0x07);
+#ifdef LED_STATUS
+    system_led_set_internal(0x07);
+#endif
+#ifdef LED_RGB
+    keyboard_led_rgb_set_internal(saved_color);
+#endif
+    app_timer_start(led_blink_timer, APP_TIMER_TICKS(200), NULL);
+}
+
 #else
 
 static void off_timer_init()
@@ -300,6 +409,9 @@ void keyboard_led_powersave(bool powersave)
 {
 }
 
+void led_display()
+{
+}
 #endif
 
 /**
@@ -311,8 +423,6 @@ void keyboard_led_init()
 #ifdef LED_RGB
     keyboard_led_rgb_init();
 #endif
-    off_timer_init();
-
 #ifdef LED_NUM
     nrf_gpio_cfg_output(LED_NUM);
 #endif
@@ -321,5 +431,52 @@ void keyboard_led_init()
 #endif
 #ifdef LED_SCLK
     nrf_gpio_cfg_output(LED_SCLK);
+#endif
+
+#ifdef LED_STATUS
+#ifdef LED_BLE
+    nrf_gpio_cfg_output(LED_BLE);
+#endif
+#ifdef LED_USB
+    nrf_gpio_cfg_output(LED_USB);
+#endif
+#ifdef LED_CHARGING
+    nrf_gpio_cfg_output(LED_CHARGING);
+#endif
+#endif
+    off_timer_init();
+}
+
+/**
+ * @brief 释放LED
+ * 
+ */
+void led_uninit(void)
+{
+#ifdef LED_RGB
+    nrf_gpio_cfg_default(LED_RGB_R);
+    nrf_gpio_cfg_default(LED_RGB_G);
+    nrf_gpio_cfg_default(LED_RGB_B);
+#endif
+#ifdef LED_NUM
+    nrf_gpio_cfg_default(LED_NUM);
+#endif
+#ifdef LED_CAPS
+    nrf_gpio_cfg_default(LED_CAPS);
+#endif
+#ifdef LED_SCLK
+    nrf_gpio_cfg_default(LED_SCLK);
+#endif
+
+#ifdef LED_STATUS
+#ifdef LED_BLE
+    nrf_gpio_cfg_default(LED_BLE);
+#endif
+#ifdef LED_USB
+    nrf_gpio_cfg_default(LED_USB);
+#endif
+#ifdef LED_CHARGING
+    nrf_gpio_cfg_default(LED_CHARGING);
+#endif
 #endif
 }
--- application/main/src/keyboard/keyboard_led.h
+++ application/main/src/keyboard/keyboard_led.h
@@ -4,6 +4,14 @@
 #include <stdint.h>
 #include "../config/keyboard_config.h"
 
+enum led_bit_usage
+{
+    // 自定义灯
+    LED_BIT_BLE,
+    LED_BIT_USB,
+    LED_BIT_CHARGING,
+};
+
 #ifdef LED_RGB
 /**
  * @brief 设置RGB灯光值
@@ -15,11 +23,31 @@ void keyboard_led_rgb_direct(uint8_t bit);
 #endif
 
 /**
- * @brief 设置LED灯光值
+ * @brief 设置键盘状态LED灯的值并启用自动熄灭
  * 
- * @param led_val 
+ * @param keyboard_led_val   LED状态值
  */
-void keyboard_led_set(uint8_t led_val);
+void keyboard_led_set(uint8_t keyboard_led_val);
+#ifdef LED_STATUS
+/**
+ * @brief 设置系统状态LED灯的状态值
+ * 
+ * @param bit 位置
+ * @param state 状态
+ */
+void system_led_set_val(enum led_bit_usage bit, bool state);
+/**
+ * @brief 设置系统状态LED灯,并启用自动熄灭
+ * 
+ */
+void system_led_set();
+/**
+ * @brief 开启系统状态LED灯
+ * 
+ */
+void system_led_on(void);
+#endif
+
 /**
  * @brief 初始化LED设置
  * 
@@ -31,11 +59,19 @@ void keyboard_led_init(void);
  * @param powersave 
  */
 void keyboard_led_powersave(bool powersave);
-
+/**
+ * @brief 启用LED状态灯显示
+ */
+void led_display();
 /**
  * @brief 关闭LED，准备进入睡眠模式
  * 
  */
 void keyboard_led_off();
+/**
+ * @brief 释放LED
+ * 
+ */
+void led_uninit(void);
 
 #endif //__KEYBOARD_LED_H_
