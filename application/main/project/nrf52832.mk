c4ac8985a0db894604c2154b5bb0f1d94d069b2f
Geno [格诺] <genokolar@gmail.com>
2019-06-23 17:51:02 +0800

    Add nrf52832 + S132  support (#6)

---
+++ application/main/project/nrf52832.mk
@@ -0,0 +1,23 @@
+SRC_FILES += \
+	$(SDK_ROOT)/modules/nrfx/mdk/gcc_startup_nrf52.S \
+	$(SDK_ROOT)/modules/nrfx/mdk/system_nrf52.c \
+
+CFLAGS += -DFLOAT_ABI_HARD
+CFLAGS += -DNRF52
+CFLAGS += -DNRF52832_XXAA
+CFLAGS += -DS132
+CFLAGS += -DCONFIG_NFCT_PINS_AS_GPIOS
+CFLAGS += -mfloat-abi=hard -mfpu=fpv4-sp-d16
+
+ASMFLAGS += -mfloat-abi=hard -mfpu=fpv4-sp-d16
+ASMFLAGS += -DFLOAT_ABI_HARD
+ASMFLAGS += -DNRF52
+ASMFLAGS += -DNRF52832_XXAA
+
+LDFLAGS += -mfloat-abi=hard -mfpu=fpv4-sp-d16
+
+SOFTDEVICE := S112
+HEAP_SIZE := 2048
+STACK_SIZE := 2048
+
+LD_NAME := nrf52832_s132.ld
