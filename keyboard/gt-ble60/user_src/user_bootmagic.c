9dce89212829821c743865914cab52d4722ed639
Geno [格诺] <genokolar@gmail.com>
2019-07-14 21:32:26 +0800

    去掉关机原因记录功能

--- keyboard/gt-ble60/user_src/user_bootmagic.c
+++ keyboard/gt-ble60/user_src/user_bootmagic.c
@@ -22,27 +22,8 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "sleep_reason.h"
 #include "usb_comm.h"
 
-void hook_bootmagic() {
-    if (!bootmagic_scan_key(BOOTMAGIC_KEY_BOOT)) {
-        bool sleep_flag = true;
-#ifdef DEBUG_SKIP_PWRON_CHECK
-        // debug状态下自动开机
-        sleep_flag = false;
-#endif
-#ifdef HAS_USB
-        // 若连接至主机则自动开机
-        if (usb_working())
-            sleep_flag = false;
-#endif
-        // 自动休眠则不需要需要使用BOOT按键开机
-        if (sleep_reason_get())
-            sleep_flag = false;
-
-        if (sleep_flag) {
-            sleep(SLEEP_NOT_PWRON);
-        }
-    }
-    
+void hook_bootmagic()
+{
     if (bootmagic_scan_key(BOOTMAGIC_KEY_ERASE_BOND)) {
         erase_bonds = true;
     }
