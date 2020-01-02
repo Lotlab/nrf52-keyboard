c607194136df2970d116bf10ee98304292d46850
jiangming1399 <jiangming1399@163.com>
2020-01-02 11:53:29 +0800

    [OLED] 增加配对码输入显示

--- application/main/src/driver/ssd1306/oled_graph.h
+++ application/main/src/driver/ssd1306/oled_graph.h
@@ -19,6 +19,7 @@ enum connection_type {
     CONN_TYPE_GAZELL,
 };
 
+void oled_clear_row(uint8_t row);
 void oled_draw_text_5x8(uint8_t row, enum text_align align, uint8_t offset, const char* str);
 void oled_draw_text_16(uint8_t row, enum text_align align, uint8_t offset, const char* str);
 void oled_draw_icons(uint8_t row, uint8_t batt, bool charging, enum connection_type conn, bool passkey, uint8_t led);
--- application/main/src/driver/ssd1306/ssd1306_oled.c
+++ application/main/src/driver/ssd1306/ssd1306_oled.c
@@ -25,6 +25,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "../../ble/ble_bas_service.h"
 #include "events.h"
 #include "keyboard_evt.h"
+#include "passkey.h"
 #include "queue.h"
 
 #include "oled_graph.h"
@@ -364,7 +365,15 @@ static void ssd1306_event_handler(enum user_event event, void* arg)
         update_status_bar();
         break;
     case USER_EVT_BLE_PASSKEY_STATE:
-        passkey_req = (param == PASSKEY_STATE_REQUIRE);
+        passkey_req = (param != PASSKEY_STATE_SEND);
+        if (param == PASSKEY_STATE_INPUT) {
+            // 显示输入的配对码
+            oled_draw_text_16(2, TEXT_ALIGN_CENTER, 0, passkey);
+        } else if (param == PASSKEY_STATE_SEND) {
+            // 清空配对码的显示
+            oled_clear_row(2);
+            oled_clear_row(3);
+        }
         update_status_bar();
         break;
     case USER_EVT_BLE_STATE_CHANGE:
