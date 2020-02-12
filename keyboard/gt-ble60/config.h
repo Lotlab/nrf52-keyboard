1764ea3faae0a5491f11a6b5ce4d70cc56203131
Geno [格诺] <genokolar@gmail.com>
2020-02-12 21:14:33 +0800

    [keyboard]更新gt ble60

--- keyboard/gt-ble60/config.h
+++ keyboard/gt-ble60/config.h
@@ -16,14 +16,15 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifndef CONFIG_H
-#define CONFIG_H
+#pragma once
 
 #include <stdint.h>
 
-/* USB Device descriptor parameter */
+/* USB和蓝牙的显示参数 */
 #define VENDOR_ID 0x1209 /* USB VID */
 #define PRODUCT_ID 0x0514 /* USB PID */
+#define CONF_VENDOR_ID 0x0114 /* 配置项目内显示的VendorID */
+#define CONF_PRODUCT_ID 0x0514 /* 配置项目内显示的ProductID */
 #define DEVICE_VER 0x0001 /* 硬件版本 */
 #define MANUFACTURER "Lotlab" /* 硬件制造商，用于蓝牙显示 */
 #define PRODUCT "GT BLE60" /* 硬件名词，用于蓝牙显示 */
@@ -33,28 +34,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBOARD_EPSIZE 8 /* 键盘上传端点大小，请不要修改 */
 #define NKRO_EPSIZE 28 /* 键盘NKRO端点大小，请不要修改 */
 
-/* key matrix size */
-#define MATRIX_ROWS 5 /* 硬件阵列行数 */
-#define MATRIX_COLS 14 /* 硬件阵列列数 */
-
-/* define if matrix has ghost */
-// #define MATRIX_HAS_GHOST /* 按键阵列是否出现Ghost Key，若没有加二极管则需要启用这个项目 */
-
-/* Set 0 if debouncing isn't needed */
-#define DEBOUNCE 5 /* 硬件消抖次数 */
-
-/* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
-#define LOCKING_SUPPORT_ENABLE
-/* Locking resynchronize hack */
-#define LOCKING_RESYNC_ENABLE
-
-/* key combination for command */
-#define IS_COMMAND() ( \
-    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
-
 // 定义Bootmagic按键
-#define BOOTMAGIC_KEY_BOOT KC_U /* 开机按键 */
-#define BOOTMAGIC_KEY_ERASE_BOND KC_E /* 删除所有绑定的按键 */
+#define BOOTMAGIC_KEY_BOOT KC_U /* 开机 */
+#define BOOTMAGIC_KEY_ERASE_BOND KC_E /* 删除所有绑定 */
 
 // 键盘省电参数
 #define SLEEP_SLOW_TIMEOUT 15 // 键盘闲置多久后转入慢速扫描模式 (s)
@@ -62,22 +44,22 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define KEYBOARD_SCAN_INTERVAL 1 // 键盘最小时间单位TICK (ms)
 #define KEYBOARD_FAST_SCAN_INTERVAL 10 // 通常模式下，多久扫描一次键盘 (ms)
 #define KEYBOARD_SLOW_SCAN_INTERVAL 100 // 慢速模式下，多久扫描一次键盘 (ms)
+#define LED_AUTOOFF_TIME 5 /* LED自动熄灭时长(s)，设为0则不自动熄灭 */
 
-// LED自动熄灭时长(s)，设为0则不自动熄灭
-#define LED_AUTOOFF_TIME 5
+// 键盘额外功能
+//#define DYNAMIC_TX_POWER /* 启用自动发射功率调整 */
+//#define PASSKEY_REQUIRED /* 需要输入配对码 */
+#define ENABLE_WATCHDOG /* 启用看门狗 */
+#define HIGH_TX_POWER /* 更改发射功率到+4dBm */
+#define MULTI_DEVICE_SWITCH  /*启用多设备切换 */
+#define KEYMAP_STORAGE /* 启用keymap存储 */
+// #define MACRO_BLOCKING_MODE /* 在宏播放时禁用其他按键输入 */
+#define MACRO_STORAGE /* 启用宏存储功能 */
+#define CONFIG_STORAGE /* 启用配置存储功能 */
 
-// 需要输入配对码
-// #define PASSKEY_REQUIRED
+// #define DEBUG_SKIP_PWRON_CHECK /*  直接开机而跳过开机条件检测，用于调试 */
 
-// 启用看门狗
-#define ENABLE_WATCHDOG
-
-// 启用keymap存储
-#define KEYMAP_STORAGE
-
-// 直接开机而跳过开机条件检测，用于调试
-// #define DEBUG_SKIP_PWRON_CHECK
-
+/* TMK固件内置功能 */
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
@@ -88,36 +70,49 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // LED 配置
 #define LED_CAPS 26  //caps led ：5 ； led1、2、3、4：19、20、27、26
 
+// ws2812 RGB 配置
+#define RGB_DI_PIN 8
+#define RGBLED_NUM 8
+#define RGBLIGHT_ANIMATIONS
+//#define RGB_PWR_PIN xx // P-mos
+//#define RGB_PWR_PIN_REVERSE 12 // N-mos
 
-//#define RGB_WS2812 8 预留
-
-// 启用 LED 状态灯
-#define LED_BLE 20
-#define LED_CHARGING 19
-#define LED_USB 27
-
+// 3灯指示配置引脚
+#define LED_STATUS_BLE 20
+#define LED_STATUS_CHARGING 19
+#define LED_STATUS_USB 27
+#define LED_POSITIVE // LED上拉驱动
 // 独立硬件按钮
 #define POWER_BUTTON 9
 
-// UART 传输配置
-#define HAS_USB
-#define UART_RXD 6
-#define UART_TXD 7
-#define UART_BAUDRATE NRF_UART_BAUDRATE_115200
+// USB UART 传输配置
+#define HAS_USB // 启用与CH554的通信支持
+#define UART_RXD 6 // UART_RX口IO
+#define UART_TXD 7 // UART_TX口IO
+//#define UART_DET 19 // UART 检测引脚，若此脚被拉低，则说明USB正在工作。若不配置则使用RX口作为检测引脚
+#define UART_BAUDRATE NRF_UART_BAUDRATE_115200 // 通信波特率，请不要修改
 
-// 电量检测配置 Pin 2
-#define BATTERY_ADC_PIN NRF_SAADC_INPUT_AIN0
+// 电量检测配置
+#define BATTERY_ADC_PIN NRF_SAADC_INPUT_AIN0 // 电量检测引脚 Pin 2
 
 // 充电检测配置
 #define PIN_CHARGING !UCC1
-#define PIN_STANDBY !UCC2
+//#define PIN_STANDBY !UCC2
 
 // 按键阵列配置
+#define MATRIX_ROWS 5 /* 硬件阵列行数 */
+#define MATRIX_COLS 14 /* 硬件阵列列数 */
+
+/* key combination for command */
+#define IS_COMMAND() ( \
+    keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)))
+
 static const uint8_t row_pin_array[MATRIX_ROWS] = {29, 25, 23, 4, 3};
 static const uint8_t column_pin_array[MATRIX_COLS] = {30, 28, 24, 22, 21, 10, 11, 12, 13, 14, 15, 16, 17, 18};
+#define ROW_IN // 键盘阵列的二极管方向是从COL->ROW
 
-#define ROW_IN // 二极管方向是从COL->ROW
+/* define if matrix has ghost */
+// #define MATRIX_HAS_GHOST /* 按键阵列是否出现Ghost Key，若没有加二极管则需要启用这个项目 */
 
-#define LED_POSITIVE // LED上拉驱动
-
-#endif
+#define DEBOUNCE 5 /* 硬件消抖次数，设置为0则不消抖 */
+#define MATRIX_SCAN_DELAY_CYCLE 48 /* 按键扫描等待IO稳定的延时时长 */
