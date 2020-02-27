1797c1a5f4773a976cf729e9994c202948af7d28
Geno [格诺] <genokolar@gmail.com>
2020-02-27 09:51:18 +0800

    [keyboard]更新Omega45（Ω45改名为Omega45）

---
+++ keyboard/Omega45/README.md
@@ -0,0 +1,13 @@
+# GT Ω45 键盘
+
+## 描述
+
+- 4x12的45%键盘
+- 一个Caps灯和三个单色状态指示灯
+- Type-C接口
+- 蓝牙主控为nRF52832
+
+## 硬件
+
+参见 [Lotkb_Keyboard_PCB](https://github.com/genokolar/Lotkb_Keyboard_PCB)。
+
---
+++ keyboard/Omega45/keymap_plain.c
@@ -0,0 +1,55 @@
+/*
+Copyright (C) 2018,2019 Jim Jiang <jim@lotlab.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#include "keymap_common.h"
+
+const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    KEYMAP(
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
+        KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_FN0, KC_RSFT,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_FN1, KC_SPC, KC_FN2, KC_FN3, KC_FN4, KC_FN5),
+
+    KEYMAP(
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+    KEYMAP(
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+
+    KEYMAP(
+        KC_GRV, KC_MYCM, KC_MAIL, KC_CALC, KC_MSEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY, KC_MPRV, KC_MNXT, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_QUOT, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_FN6, KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_SLSH, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+
+};
+const action_t fn_actions[] = {
+    /* Poker Layout */
+    ACTION_LAYER_TAP(3, KC_UP),
+    ACTION_LAYER_TAP(1, KC_SPC),
+    ACTION_LAYER_TAP(2, KC_DEL),
+    ACTION_MODS_TAP_KEY(MOD_RALT, KC_LEFT),
+    ACTION_LAYER_TAP(3, KC_DOWN),
+    ACTION_MODS_TAP_KEY(MOD_RCTL, KC_RGHT),
+    ACTION_FUNCTION(7)
+};
