96577eb41a009ebca9d397b1b314a057605f1b9d
Geno [格诺] <genokolar@gmail.com>
2020-10-20 20:31:24 +0800

    [keyboard]所有键盘启用bootcheck功能

--- keyboard/gt-ble60/d/rules.mk
+++ keyboard/gt-ble60/d/rules.mk
@@ -4,11 +4,11 @@ NRF_CHIP := nrf52832
 
 # 配置项：功能选项
 # BOOTMAGIC_ENABLE = yes  # 启用Bootmagic
-# BOOTCHECK_ENABLE = yes  # 启用Bootchek
+BOOTCHECK_ENABLE = yes    # 启用Bootchek
 EXTRAKEY_ENABLE = yes	  # 启用媒体键功能
 MOUSEKEY_ENABLE = yes     # 启用鼠标键功能
 NKRO_ENABLE = yes	      # 启用USB的全键无冲功能
-USB_6KRO_ENABLE = yes   # 启用USB的六键无冲功能
+USB_6KRO_ENABLE = yes     # 启用USB的六键无冲功能
 COMMAND_ENABLE = yes      # 启用调试和配置的命令
 ifeq (nrf52832,$(strip $(NRF_CHIP)))
 # RGBLIGHT_ENABLE = yes       #启用RGB灯
