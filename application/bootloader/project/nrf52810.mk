d6671817b85466203c4c3c29711c251f7e36c4f5
Geno [格诺] <genokolar@gmail.com>
2020-11-26 15:11:23 +0800

    【DFU】修正nrf52832编译问题

--- application/bootloader/project/nrf52810.mk
+++ application/bootloader/project/nrf52810.mk
@@ -1,6 +1,11 @@
 SRC_FILES += \
 	$(SDK_ROOT)/modules/nrfx/mdk/gcc_startup_nrf52810.S \
 	$(SDK_ROOT)/modules/nrfx/mdk/system_nrf52810.c
+	
+# Libraries common to all targets
+LIB_FILES += \
+	$(SDK_ROOT)/external/nrf_oberon/lib/cortex-m4/soft-float/liboberon_2.0.7.a \
+	$(SDK_ROOT)/external/micro-ecc/nrf52nf_armgcc/armgcc/micro_ecc_lib_nrf52.a \
 
 CFLAGS += -DFLOAT_ABI_SOFT
 CFLAGS += -DNRF52810_XXAA
--- application/bootloader/project/nrf52832.mk
+++ application/bootloader/project/nrf52832.mk
@@ -15,11 +15,21 @@ ifndef NRF52_DISABLE_FPU
 	ASMFLAGS += -mfloat-abi=hard -mfpu=fpv4-sp-d16
 	ASMFLAGS += -DFLOAT_ABI_HARD
 	LDFLAGS += -mfloat-abi=hard -mfpu=fpv4-sp-d16
+	
+# Libraries common to all targets
+LIB_FILES += \
+	$(SDK_ROOT)/external/nrf_oberon/lib/cortex-m4/hard-float/liboberon_2.0.7.a \
+	$(SDK_ROOT)/external/micro-ecc/nrf52hf_armgcc/armgcc/micro_ecc_lib_nrf52.a
 else
 	CFLAGS += -DFLOAT_ABI_SOFT
 	CFLAGS += -mfloat-abi=soft
 	ASMFLAGS += -mfloat-abi=soft
 	ASMFLAGS += -DFLOAT_ABI_SOFT
+	
+# Libraries common to all targets
+LIB_FILES += \
+	$(SDK_ROOT)/external/nrf_oberon/lib/cortex-m4/soft-float/liboberon_2.0.7.a \
+	$(SDK_ROOT)/external/micro-ecc/nrf52nf_armgcc/armgcc/micro_ecc_lib_nrf52.a
 endif
 
 SOFTDEVICE := S132
