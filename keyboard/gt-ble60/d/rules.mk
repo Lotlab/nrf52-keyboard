12fe0bffbfb9530fa6377a944a0ea482fd3e1cea
Geno [格诺] <genokolar@gmail.com>
2020-12-06 16:47:41 +0800

    【keyboard】更改ws2812相关

--- keyboard/gt-ble60/d/rules.mk
+++ keyboard/gt-ble60/d/rules.mk
@@ -11,6 +11,6 @@ NKRO_ENABLE = yes	      # 启用USB的全键无冲功能
 USB_6KRO_ENABLE = yes     # 启用USB的六键无冲功能
 COMMAND_ENABLE = yes      # 启用调试和配置的命令
 ifeq (nrf52832,$(strip $(NRF_CHIP)))
-# RGBLIGHT_ENABLE = yes       #启用RGB灯
+# WS2812_ENABLE = yes       #启用WS2812灯
 endif
 THREE_LED_STATUS = yes  #启用键盘运行状态灯
