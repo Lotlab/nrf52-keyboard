48606fa4226a0cd1ac33520d455594e2dcbba44f
Comic Chang <1257169+comicchang@users.noreply.github.com>
2021-01-03 11:32:09 +0800

    make keymap more human-readable (#65)

--- keyboard/Omega84/keymap_plain.c
+++ keyboard/Omega84/keymap_plain.c
@@ -18,94 +18,88 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "keymap_common.h"
 #include "keyboard_fn.h"
 
+#define KC_____ KC_TRNS
+
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	KEYMAP(
-		KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, 
-		KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, 
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, 
-		KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, 
-		KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RALT, KC_FN0, 
-		KC_HOME, KC_BSPC, KC_EQL, KC_MINS, KC_0, KC_INS, KC_PSCR, KC_F12, KC_F11, KC_F10, 
-		KC_DEL, KC_PGDN, KC_ENT, KC_QUOT, KC_SCLN, KC_PGUP, KC_BSLS, KC_RBRC, KC_LBRC, KC_P, 
-		KC_RGHT, KC_DOWN, KC_LEFT, KC_RCTL, KC_END, KC_UP, KC_RSFT, KC_SLSH),
+    /* Keymap 0: Default Layer
+     * ,---------------------------------------------------------------.
+     * |Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|PSc|Ins|Del|
+     * |---------------------------------------------------------------|
+     * |  `|  1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Hom|
+     * |---------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|    \|PgU|
+     * |---------------------------------------------------------------|
+     * |Caps  |  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return  |PgD|
+     * |---------------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift |Up |End|
+     * |---------------------------------------------------------------|
+     * |Ctrl|Gui |Alt |      Space          |Alt|Fn0|Ctl|Left|Down|Rght|
+     * `---------------------------------------------------------------'
+     */
+    KEYMAP(
+        ESC,  F1,   F2,   F3,  F4,   F5,  F6,   F7,   F8,   F9,   F10,  F11,  F12,  PSCR, INS,  DEL,
+        GRV,  1,    2,    3,   4,    5,   6,    7,    8,    9,    0,    MINS, EQL,  BSPC,       HOME,
+        TAB,  Q,    W,    E,   R,    T,   Y,    U,    I,    O,    P,    LBRC, RBRC, BSLS,       PGUP,
+        CAPS, A,    S,    D,   F,    G,   H,    J,    K,    L,    SCLN, QUOT,       ENT,        PGDN,
+        LSFT, Z,    X,    C,   V,    B,   N,    M,    COMM, DOT,  SLSH, RSFT,             UP,   END,
+        LCTL, LGUI, LALT,                 SPC,              RALT, FN0,  RCTL,       LEFT, DOWN, RGHT),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_MYCM, KC_MAIL, KC_CALC, KC_MSEL, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MUTE),
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, MYCM, MAIL, CALC, MSEL, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, BTN1, MS_U, BTN2, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, MS_L, MS_D, MS_R, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, VOLD, VOLU, MUTE, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
 
-	KEYMAP(
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
+    KEYMAP(
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,       ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____,
+        ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____,             ____, ____,
+        ____, ____, ____,                   ____,             ____, ____, ____,       ____, ____, ____),
+
 };
 
 const action_t fn_actions[] = {
