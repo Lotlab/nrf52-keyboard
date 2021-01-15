0256aa26635845b28c11eeee09ee2eb579a0b40e
Jim Jiang <jim@lotlab.org>
2021-01-15 12:55:08 +0800

    添加新的宏指令：循环

---
+++ application/main/src/keyboard/action_macro_extend.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <action_macro.h>
+
+enum macro_command_id_ext{
+    // 循环当前宏
+    // 再按一次取消循环
+    REPEAT = 0x80
+};
--- application/main/src/keyboard/macro_player.h
+++ application/main/src/keyboard/macro_player.h
@@ -11,6 +11,12 @@ void macro_play_timer_init(void);
  * 检查宏是否播放完毕
  */
 bool macro_queue_empty(void);
+
+/**
+ * @brief 取消正在播放的所有宏
+ * 
+ */
+void macro_cancle(void);
 #else
 #define macro_play_timer_init()
 #define macro_queue_empty() true
