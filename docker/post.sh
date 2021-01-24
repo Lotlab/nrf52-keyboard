dc34bf8723de30e722023356926a5beff50d9995
Jim Jiang <jim@lotlab.org>
2021-01-24 19:58:42 +0800

    修正docker sed路径出错的问题

--- docker/post.sh
+++ docker/post.sh
@@ -3,5 +3,5 @@
 cd /work
 
 cp template/Makefile.posix.template template/Makefile.posix
-sed -i "s/\/usr\/local/\/opt\/g" template/Makefile.posix
+sed -i "s/\/usr\/local/\/opt/g" template/Makefile.posix
 cp template/Makefile.posix SDK/components/toolchain/gcc/Makefile.posix
