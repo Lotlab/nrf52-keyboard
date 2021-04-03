7c2caa2b5e0d48ae245cc0fc3971fc88dc3ccdb7
Jim Jiang <jim@lotlab.org>
2021-04-03 19:35:32 +0800

    修正bootloader在部分系统下编译不正确的问题；添加新操作: merge_bootloader_all

--- application/main/project/bl.mk
+++ application/main/project/bl.mk
@@ -9,10 +9,10 @@ export NRF_CHIP SOFTDEVICE NRF52_DISABLE_FPU CONFIG_H
 BL_MAKE_ARGS = -C $(BOOTLOADER_DIR)  
 
 ifdef OUTPUT_DIRECTORY
-BL_MAKE_ARGS += OUTPUT_DIRECTORY=$(abspath $(OUTPUT_DIRECTORY)) 
+BL_MAKE_ARGS += OUTPUT_DIRECTORY=$(shell realpath --relative-to $(BOOTLOADER_DIR) $(OUTPUT_DIRECTORY))
 endif
 ifdef CONFIG_H_DIR
-BL_MAKE_ARGS += CONFIG_H_DIR=$(abspath $(CONFIG_H_DIR)) 
+BL_MAKE_ARGS += CONFIG_H_DIR=$(shell realpath --relative-to $(BOOTLOADER_DIR) $(CONFIG_H_DIR))
 endif
 
 bootloader: 
@@ -23,3 +23,7 @@ flash_bootloader:
 
 pyocd_flash_bootloader:
 	@make $(BL_MAKE_ARGS) pyocd_flash
+
+merge_bootloader_all: bootloader merge_all
+	@echo Merging program, signature, SoftDevice and bootloader to $(OUTPUT_DIRECTORY)/nrf52_all.hex
+	mergehex -m $(OUTPUT_DIRECTORY)/nrf52_bootloader.hex $(OUTPUT_DIRECTORY)/nrf52_kbd_sign_with_sd.hex -o $(OUTPUT_DIRECTORY)/nrf52_all.hex
