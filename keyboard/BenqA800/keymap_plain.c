d92011cf7329fe9fb191a44be75c2a8be9598705
panbingzhen <panbingzhen@ringle.com>
2021-05-24 17:45:34 +0800

    Change keyboard layout for PCB v2.0

--- keyboard/BenqA800/keymap_plain.c
+++ keyboard/BenqA800/keymap_plain.c
@@ -22,14 +22,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* 0: qwerty */
     KEYMAP( 
-        NO,   ESC, F12,  NO,   H,  F6,   G,  NO,QUOT,  F5,  F4,  P0,PDOT,  NO,  NO,LALT,  NO,  NO,MPRV,
-        LSFT, TAB,CAPS,  F3,   Y,  F7,   T,RBRC,LBRC,BSPC,  P4,  P5,  P6,PPLS,  NO,  NO,  NO,  NO,MNXT,
-        NO,   GRV,  F1,  F2,  F9,  F8,   5, EQL,MINS,   6, INS, DEL,PGUP,HOME,  NO,  NO,LCTL,  NO,WHOM,
-        NO,     1, F11,PSCR, F10,   9,   4,   8,   0,   7,   2,   3,PGDN, END,  NO,  NO,  NO,LGUI,MAIL,
-        RSFT,   Q,   W,PAUS,   U,   O,   R,   I,   P,  NO,  P7,  P8,  P9,   E,  NO,  NO,  NO,  NO,WFAV,
-        NO,     A,   S,   D,   J,   L,   F,   K,SCLN,BSLS,  P1,  P2,  P3,PENT,RGUI,  NO,  NO,  NO,  NO,
-        NO,     Z,   X,   C,   M, DOT,   V,COMM,  NO, ENT,NLCK,RGHT,PAST,LEFT,  NO,  NO,RCTL,  NO,  NO,
-        NO,    NO,  NO, SPC,   N,  NO,   B,  NO,SLSH,  UP,PSLS,DOWN,PMNS, APP,  NO,RALT,  NO,  NO,  NO)
+        NO,   ESC, F12,  NO,   H,  F6,   G,WFAV,QUOT,  F5,  F4,  P0,PDOT,  NO,  NO,LALT,  NO,
+        LSFT, TAB,CAPS,  F3,   Y,  F7,   T,RBRC,LBRC,BSPC,  P4,  P5,  P6,PPLS,  NO,  NO,  NO,
+        NO,   GRV,  F1,  F2,  F9,  F8,   5, EQL,MINS,   6, INS, DEL,PGUP,HOME,  NO,  NO,LCTL,
+        NO,     1, F11,PSCR, F10,   9,   4,   8,   0,   7,   2,   3,PGDN, END,LGUI,  NO,  NO,
+        RSFT,   Q,   W,PAUS,   U,   O,   R,   I,   P,  NO,  P7,  P8,  P9,   E,  NO,  NO,  NO,
+        NO,     A,   S,   D,   J,   L,   F,   K,SCLN,BSLS,  P1,  P2,  P3,PENT,RGUI,  NO,  NO,
+        NO,     Z,   X,   C,   M, DOT,   V,COMM,  NO, ENT,NLCK,RGHT,PAST,LEFT,  NO,  NO,RCTL,
+        NO,  MPRV,MNXT, SPC,   N,WHOM,   B,MAIL,SLSH,  UP,PSLS,DOWN,PMNS, APP,  NO,RALT,  NO)
 };
 const action_t fn_actions[] = {
     /* Poker Layout */
