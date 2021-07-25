fb76e70c61272f339cf2cae4b577bb9da0cd990d
Geno [格诺] <genokolar@gmail.com>
2021-07-25 09:59:36 +0800

    Revert "适当提高蓝牙最高MTU"
    This reverts commit 31518ec018b8504b9009f2f32caf587226d13a65.

--- application/main/src/config/sdk_config.h
+++ application/main/src/config/sdk_config.h
@@ -9875,7 +9875,7 @@
 // <i> Requested BLE GAP data length to be negotiated.
 
 #ifndef NRF_SDH_BLE_GAP_DATA_LENGTH
-#define NRF_SDH_BLE_GAP_DATA_LENGTH 76
+#define NRF_SDH_BLE_GAP_DATA_LENGTH 27
 #endif
 
 // <o> NRF_SDH_BLE_PERIPHERAL_LINK_COUNT - Maximum number of peripheral links. 
@@ -9904,7 +9904,7 @@
 
 // <o> NRF_SDH_BLE_GATT_MAX_MTU_SIZE - Static maximum MTU size. 
 #ifndef NRF_SDH_BLE_GATT_MAX_MTU_SIZE
-#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE 72
+#define NRF_SDH_BLE_GATT_MAX_MTU_SIZE 23
 #endif
 
 // <o> NRF_SDH_BLE_GATTS_ATTR_TAB_SIZE - Attribute Table size in bytes. The size must be a multiple of 4. 
