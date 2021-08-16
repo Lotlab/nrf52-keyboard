006550119efea36748583584be73646febb964ef
Geno [格诺] <genokolar@gmail.com>
2021-08-16 11:39:42 +0800

    修改Makefile：生成完整固件包

--- application/main/project/app.mk
+++ application/main/project/app.mk
@@ -384,6 +384,11 @@ merge_all: setting
 	@echo Merging program, signature and SoftDevice $(SOFTDEVICE_NAME) to $(NRF_MERGE_ALL_NAME)
 	mergehex -m $(SOFTDEVICE_PATH) $(OUTPUT_DIRECTORY)/nrf52_settings.hex $(OUTPUT_DIRECTORY)/nrf52_kbd.hex -o $(NRF_MERGE_ALL_NAME)
 
+make_all: bootloader
+	@echo make: $(NRF_MERGE_ALL_NAME)
+	mergehex -m $(SOFTDEVICE_PATH) $(OUTPUT_DIRECTORY)/nrf52_settings.hex $(OUTPUT_DIRECTORY)/nrf52_kbd.hex -o $(OUTPUT_DIRECTORY)/nrf52_kbd_sign_with_sd.hex
+	mergehex -m $(OUTPUT_DIRECTORY)/$(NRF_CHIP)_bootloader.hex $(OUTPUT_DIRECTORY)/nrf52_kbd_sign_with_sd.hex -o $(NRF_MERGE_ALL_NAME)
+
 pyocd_flash_all: merge_bootloader_all
 	@echo Flashing: $(OUTPUT_DIRECTORY)/nrf52_all.hex
 	pyocd flash -t nrf52 -e sector -f 2M $(OUTPUT_DIRECTORY)/nrf52_all.hex
