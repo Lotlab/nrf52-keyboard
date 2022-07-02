7eec53edbb3ad1e876939f50de404836fd3f7004
Leo Deng <myst.dg@gmail.com>
2022-07-02 12:40:31 +0800

    枚举定义到h文件方便别处引入

--- application/main/src/driver/ws2812/rgblight_ctrl.c
+++ application/main/src/driver/ws2812/rgblight_ctrl.c
@@ -1,20 +1,6 @@
 #include "keyboard_fn.h"
 #include "rgb.h"
 
-enum RGBLightFunc {
-    RGBLIGHT_TOGGLE, // 开关
-    RGBLIGHT_MODE_INCREASE, // 模式+
-    RGBLIGHT_MODE_DECREASE, // 模式-
-    RGBLIGHT_HUE_INCREASE, // 色相+
-    RGBLIGHT_HUE_DECREASE, // 色相-
-    RGBLIGHT_SAT_INCREASE, // 饱和度+
-    RGBLIGHT_SAT_DECREASE, // 饱和度-
-    RGBLIGHT_VAL_INCREASE, // 明度+
-    RGBLIGHT_VAL_DECREASE, // 明度-
-    RGBLIGHT_SPEED_INCREASE, // 速度+
-    RGBLIGHT_SPEED_DECREASE, // 速度-
-};
-
 static void rgblight_fn_handler(keyrecord_t* record, uint8_t id, uint8_t opt)
 {
     if (id == RGBLIGHT_CONTROL && record->event.pressed) {
